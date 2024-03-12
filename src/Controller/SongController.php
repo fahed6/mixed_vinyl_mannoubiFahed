3b6a2616e00b54a75a7bbd4e13cfff61cc053d43
MANNOUBI_FAHED <mannoubi.fahed7@gmail.com>
2024-03-12 12:42:27 +0100

    Never gonna let you go...

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -4,19 +4,53 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
-    public function getSong( int $id): Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
-        // TODO query the database
-        $song = [
-            'id' => $id,
-            'name' => 'Waterfalls',
-            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        $songs = [
+            1 => [
+                'id' => 1,
+                'name' => 'Bohemian Rhapsody',
+                'url' => 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
+            ],
+            2 => [
+                'id' => 2,
+                'name' => 'Hotel California',
+                'url' => 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+            ],
+            3 => [
+                'id' => 3,
+                'name' => 'Imagine',
+                'url' => 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3',
+            ],
+            4 => [
+                'id' => 4,
+                'name' => 'Set The World On Fire',
+                'url' => 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3',
+            ],
+            5 => [
+                'id' => 5,
+                'name' => 'Your',
+                'url' => 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3',
+            ],
+            6 => [
+                'id' => 6,
+                'name' => 'Parfum',
+                'url' => 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3',
+            ],
         ];
 
-        return $this->json($song);
+        if (array_key_exists($id, $songs)) {
+            $song = $songs[$id];
+            $logger->info('Returning API response for song {song}', [
+                'song' => $id,
+            ]);
+
+            return $this->json($song);
+        } 
     }
 }
